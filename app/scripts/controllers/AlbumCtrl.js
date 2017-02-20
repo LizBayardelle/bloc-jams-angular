098a069292305c5bb788033cb5ae9b7abedcc073
Liz Bayardelle <lizbayardelle@gmail.com>
2017-02-20 11:08:15 -0800

    Step 7

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,12 +1,13 @@
 (function() {
-    function AlbumCtrl(Fixtures) {
+    function AlbumCtrl(Fixtures, SongPlayer) {
     	var album = this;
 
     	album.albumData = Fixtures.getAlbum();
+    	album.songPlayer = SongPlayer;
 
     }
  
     angular
         .module('blocJams')
-        .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
+        .controller('AlbumCtrl', ['Fixtures', 'SongPlayer', AlbumCtrl]);
 })();
