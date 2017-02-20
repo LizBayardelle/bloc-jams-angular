2b0920153c459a29ef2ce3c7c7ec2fb58a52d851
Liz Bayardelle <lizbayardelle@gmail.com>
2017-02-20 11:12:45 -0800

    Step 7 Assignment

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -29,11 +29,20 @@
 		    currentSong = song;
 		};
 
+		/**
+		* @function playSong
+		* @desc Plays the current Buzz object
+		* @param {Object} song
+		*/
+		var playSong = function(song) {
+			currentBuzzObject.play();
+			song.playing = true;
+		};
+
         SongPlayer.play = function(song) {
         	if (currentSong !== song) {
         		setSong(song);
-		        currentBuzzObject.play(); 
-		        song.playing = true;
+		        playSong(song);
 
         	} else if (currentSong === song) {
 
