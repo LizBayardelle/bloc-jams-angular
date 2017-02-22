6001d549656d793ca9eec6a05816454200677876
Liz Bayardelle <lizbayardelle@gmail.com>
2017-02-22 14:45:43 -0800

    Step 10

--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -1,5 +1,5 @@
 (function() {
-    function SongPlayer(Fixtures) {
+    function SongPlayer($rootScope, Fixtures) {
         var SongPlayer = {};
 
         /**
@@ -27,6 +27,12 @@
 		    currentBuzzObject = new buzz.sound(song.audioUrl, {
 		        formats: ['mp3'],
 		        preload: true
+		    });
+
+		    currentBuzzObject.bind('timeupdate', function() {
+		        $rootScope.$apply(function() {
+		            SongPlayer.currentTime = currentBuzzObject.getTime();
+		        });
 		    });
 		 
 		    SongPlayer.currentSong = song;
@@ -57,6 +63,12 @@
 		};
 
         SongPlayer.currentSong = null;
+
+        /**
+		* @desc Current playback time (in seconds) of currently playing song
+		* @type {Number}
+		*/
+		SongPlayer.currentTime = null;
 
         SongPlayer.play = function(song) {
         	song = song || SongPlayer.currentSong;
@@ -113,12 +125,21 @@
 		    }
 		};
 
-
+		/**
+		* @function setCurrentTime
+		* @desc Set current time (in seconds) of currently playing song
+		* @param {Number} time
+		*/
+		SongPlayer.setCurrentTime = function(time) {
+		    if (currentBuzzObject) {
+		        currentBuzzObject.setTime(time);
+		    }
+		};
 
         return SongPlayer;
     }
  
     angular
         .module('blocJams')
-        .factory('SongPlayer', ['Fixtures', SongPlayer]);
+        .factory('SongPlayer', ['$rootScope', 'Fixtures', SongPlayer]);
 })();
